6660261b8a64eb3e421ac117c422d34c3d2aecf3
Jan Funke <funke@ini.ch>
2013-07-22 12:14:29 +0200

    added convinience macros for typelist creation

--- typelist.h
+++ typelist.h
@@ -1,3 +1,5 @@
+#ifndef UTIL_TYPELIST_H__
+#define UTIL_TYPELIST_H__
 
 class TypeNone {
 
@@ -7,6 +9,11 @@ public:
 	typedef TypeNone Tail;
 };
 
+/**
+ * A typelist to give several types as a single template argument.
+ * 
+ * Usage: TypeList<int, TypeList<float, ... > >
+ */
 template <typename H, typename T = TypeNone >
 class TypeList {
 
@@ -16,4 +23,17 @@ public:
 	typedef T Tail;
 };
 
-// Usage: TypeList<int, TypeList<float, ... > >
+// convenience macros for the easy creation of typelists
+#define TYPELIST_1(x1) TypeList<x1>
+#define TYPELIST_2(x1, x2) TypeList<x1, TypeList<x2> >
+#define TYPELIST_3(x1, x2, x3) TypeList<x1, TypeList<x2, TypeList<x3> > >
+#define TYPELIST_4(x1, x2, x3, x4) TypeList<x1, TypeList<x2, TypeList<x3, TypeList<x4> > > >
+#define TYPELIST_5(x1, x2, x3, x4, x5) TypeList<x1, TypeList<x2, TypeList<x3, TypeList<x4, TypeList<x5> > > > >
+#define TYPELIST_6(x1, x2, x3, x4, x5, x6) TypeList<x1, TypeList<x2, TypeList<x3, TypeList<x4, TypeList<x5, TypeList<x6> > > > > >
+#define TYPELIST_7(x1, x2, x3, x4, x5, x6, x7) TypeList<x1, TypeList<x2, TypeList<x3, TypeList<x4, TypeList<x5, TypeList<x6, TypeList<x7> > > > > > >
+#define TYPELIST_8(x1, x2, x3, x4, x5, x6, x7, x8) TypeList<x1, TypeList<x2, TypeList<x3, TypeList<x4, TypeList<x5, TypeList<x6, TypeList<x7, TypeList<x8> > > > > > > >
+#define TYPELIST_9(x1, x2, x3, x4, x5, x6, x7, x8, x9) TypeList<x1, TypeList<x2, TypeList<x3, TypeList<x4, TypeList<x5, TypeList<x6, TypeList<x7, TypeList<x8, TypeList<x9> > > > > > > > >
+#define TYPELIST_10(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10) TypeList<x1, TypeList<x2, TypeList<x3, TypeList<x4, TypeList<x5, TypeList<x6, TypeList<x7, TypeList<x8, TypeList<x9, TypeList<x10> > > > > > > > > >
+
+#endif // UTIL_TYPELIST_H__
+
