889974d66936b4883ffdbbfe251304a2502d39a7
Jan Funke <funke@ini.ch>
2015-04-06 17:43:48 +0200

    checkin of ray.hpp

---
+++ ray.hpp
@@ -0,0 +1,41 @@
+#ifndef UTIL_RAY_H__
+#define UTIL_RAY_H__
+
+#include "point.hpp"
+
+namespace util {
+
+template <typename T, int N>
+class ray {
+
+public:
+
+	ray() {}
+
+	ray(const point<T,N>& position, const point<T,N>& direction) :
+		_position(position),
+		_direction(direction) {}
+
+	const point<T,N>& position() const { return _position; }
+	point<T,N>& position() { return _position; }
+
+	const point<T,N>& direction() const { return _direction; }
+	point<T,N>& direction() { return _direction; }
+
+private:
+
+	point<T,N> _position;
+	point<T,N> _direction;
+};
+
+} // namespace util
+
+template <typename T, int N>
+std::ostream& operator<<(std::ostream& os, const util::ray<T,N>& ray) {
+
+	os << "{" << ray.position() << "->" << ray.direction() << "}";
+	return os;
+}
+
+#endif // UTIL_RAY_H__
+
